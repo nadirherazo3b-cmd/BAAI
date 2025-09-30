454e91e3d7f216b3e3dba291238156fb98c38f5c
Nadir <nadir.herazo.3b@gmail.com>
2025-10-01 01:08:40 +0800

    cambio

---
+++ Apuntes y prubas.py
@@ -0,0 +1,29 @@
+#print("Este es un cambio de prueba") 
+#print ("Esta es otra prueba de que funciona git y esta sincronizado con vs code")
+#print ("ultima comprobacion)")
+#print ("prueba 27/09")
+#print ("prueba 31/09)")
+
+# Example operations in Python
+
+# 1. Input
+X = 8
+Y = 3
+
+# 2. Process
+sumar = X + Y
+restar = X - Y
+multiplicar = X * Y
+division = X / Y
+division_entera = X // Y
+modulo = X % Y
+potencia = X ** Y
+
+# 3. Output
+print(f"Suma: {sumar}")
+print(f"Resta: {restar}")
+print(f"Multiplicación: {multiplicar}")
+print(f"División: {division}")
+print(f"División entera: {division_entera}")
+print(f"Módulo: {modulo}")
+print(f"Potencia: {potencia}")

c3156454608239b03911a910a945891dce37fbe6
Nadir <nadir.herazo.3b@gmail.com>
2025-10-08 16:16:14 +0800

    operaciones python

--- Nadir_Sum.py
+++ Nadir_Sum.py
@@ -5,7 +5,20 @@
 #
 
 # 1. Input
+#X = int (input ('First number: '))
+#Y = int (input ('Second number: '))
+
+X = int (input ('First number: '))
+Y = int (input ('Second number: '))
 
 # 2. Process
+#Sum = X + Y
+X1 = int(X)
+Y1 = int(Y)
+
+Sum = X1 + Y1
+Sum1 = X1 + Y1
 
 # 3. Output
+print (f'Result {Sum}')
+print (f'Result {Sum1}')
